15fd894ec2f4dc7caa9cdf6b11510d762d6d8c74
will <will@Will-Desktop.(none)>
2009-02-24 21:53:05 +1100

    Greatly simplified Component.

--- src/Component.rb
+++ src/Component.rb
@@ -12,66 +12,52 @@ require 'erb'
 
 class Component
 
-  attr_reader :kind, :params
+  attr_accessor :kind, :params, :children, :template, :decorators
 
   #
   # Constructor.
   #
-  # Takes the set of parameters for the component, by default.
-  #
-  def initialize( kind, params )
+  def initialize( kind = "Component", params = {}, children = [], template = "", decorators = [])
     @kind = kind
     @params = params
-    
-    init(params)
+    @children = children
+    @template = template
+    @decorators = decorators
   end
-  
-  
-  def init(params)
-  end
-  
   
   #
   # Renders the component.
   #
   # Takes as parameter the format to render in.
   #
-  def render( format )
-    e = ERB.new(template)
+  def render( format = :xhtml )
+    # Render the component
+    output = render_self(format)
+    
+    # Decorate the output
+    for decorator in decorators
+      decorator.decorate(output, format)
+    end
+  end
+  
+  #
+  # Renders this component alone.
+  #
+  def render_self( format = :xhtml )
+    e = ERB.new(@template)
     e.result( binding() )
   end
 
-
   #
-  # Returns the default template (nothing).
-  #
-  def template
-    ""
-  end
-
-  
-  #
-  # Loads a component from a YAML tree.
-  #
-  def self.load( kind, params = nil )
-    #TODO: proper searching for files, etc
-    
-    # Get the class to instantiate
-    begin
-      cls = const_get(kind)
-    rescue NameError
-      cls = Component
+  # Renders all children of this component, in order, and returns the resultant
+  # string.
+  #  
+  def render_children( format = :xhtml )
+    @children.each do |child|
+      output += child.render(format)
     end
     
-    cls.new(kind, params)
-  end
-  
-  def inspect
-    "<#{kind}, #{params.inspect}>"
-  end
-  
-  def is_kind?(kind)
-    @kind == kind
+    return output
   end
 end
 
