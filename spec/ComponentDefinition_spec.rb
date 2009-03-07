4fdef6f39ca0fdb2f3a36a996c73511f2b9dbc64
William Madden <w.a.madden@gmail.com>
2009-03-07 15:07:26 +1100

    Finished ComponentDefinition unit tests.

--- spec/ComponentDefinition_spec.rb
+++ spec/ComponentDefinition_spec.rb
@@ -21,6 +21,7 @@
 ################################################################################
 
 require 'src/ComponentDefinition.rb'
+require 'src/Component.rb'
 
 include Bob
 
@@ -37,25 +38,37 @@ describe ComponentDefinition do
     @sample_template = "sample template"
     @sample_decorators = [:dec1, :dec2, :dec3]
     
+    @new_params = {:d => :e, :e => :f, :f => :g}
+    @new_children = [:child4, :child5, :child6]
+    @new_decorators = [:dec4, :dec5, :dec6]
+    @new_template = "new template"
+    
     @sample_defaults = {
       :children => @sample_children,
       :parameters => @sample_params,
       :template => @sample_template,
       :decorators => @sample_decorators
     }
+    
+    @kind = 'SomeComponent'
+    @parent = 'Component'
+    @ancestors = [@kind, @parent]
+    @concrete_class = Component
+    
+    @new_defaults = {
+      :children => @new_children,
+      :parameters => @new_params,
+      :template => @new_template,
+      :decorators => @new_decorators
+    }
   end
 
   it "should be correctly initialized from a defaults hash" do
-    kind = 'SomeComponent'
-    parent = 'Component'
-    ancestors = [kind, parent]
-    concrete_class = 'Component'
-    
-    definition = ComponentDefinition.new(kind, @sample_defaults, ancestors, concrete_class)
+    definition = ComponentDefinition.new(@kind, @sample_defaults, @ancestors, @concrete_class)
 
-    definition.kind.should == kind
-    definition.ancestors.should == ancestors
-    definition.parent.should == parent
+    definition.kind.should == @kind
+    definition.ancestors.should == @ancestors
+    definition.parent.should == @parent
     definition.defaults.should == @sample_defaults
     definition.parameters.should == @sample_params
     definition.children.should == @sample_children
@@ -63,7 +76,63 @@ describe ComponentDefinition do
     definition.template.should == @sample_template
   end
   
-  it "should merge defaults intelligently"
+  it "should merge defaults intelligently" do
+    # Children overwrite defaults
+    # Parameters merge (using Hash.merge)
+    # Decorators overwrite
+    # Template overwrites
+    child_defs = @new_defaults
+    parent_defs = @sample_defaults
+    
+    result = ComponentDefinition.merge_defaults( child_defs, parent_defs )
+    result[:children].should == @new_children
+    result[:parameters].should == @sample_params.merge(@new_params)
+    result[:decorators].should == @new_decorators
+    result[:template].should == @new_template
+  end
   
-  it "should instantiate components with its defaults"
+  it "should instantiate components with its defaults" do
+    definition = ComponentDefinition.new(@kind, @sample_defaults, @ancestors, @concrete_class)
+    component = definition.instantiate
+    
+    component.template.should == definition.template
+    component.children.should == definition.children
+    component.decorators.should == definition.decorators
+    component.kind.should == definition.kind
+    component.params.should == definition.parameters
+  end
+  
+  it "should instantiate components with merged defaults" do
+    definition = ComponentDefinition.new(@kind, @sample_defaults, @ancestors, @concrete_class)
+    component = definition.instantiate(@new_params, @new_children, @new_template, @new_decorators)
+    
+    component.template.should == @new_template
+    component.children.should == @new_children
+    component.decorators.should == @new_decorators
+    component.kind.should == definition.kind
+    component.params.should == @sample_params.merge(@new_params)
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
