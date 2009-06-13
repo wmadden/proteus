cd19025110768bc19e962cce6d239829128485fa
Will Madden <w.a.madden@gmail.com>
2009-06-13 15:05:08 +1000

    Added DefinitionParser spec stub.

---
+++ spec/DefinitionParser_spec.rb
@@ -0,0 +1,33 @@
+################################################################################
+# (C) Copyright 2009 William Madden
+# 
+# This file is part of Bob.
+# 
+# Bob is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Bob is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# Bob.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+require File.expand_path( 'src/InputParser.rb' )
+
+include Bob
+
+
+describe DefinitionParser do
+  
+  it "should use the class parser to parse the file"
+  
+  it "should use the input parser to parse the class's values"
+  
+  it "should use the definition helper to get the parent"
+  
+end
+
