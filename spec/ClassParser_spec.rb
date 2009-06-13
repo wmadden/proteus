276f53b8e4001f4253e8591aed435f918ce78f65
Will Madden <w.a.madden@gmail.com>
2009-06-13 14:29:52 +1000

    Finished ClassParser unit tests.

--- spec/ClassParser_spec.rb
+++ spec/ClassParser_spec.rb
@@ -29,9 +29,78 @@ describe ClassParser do
   #  
   #-----------------------------------------------------------------------------
   
-  DEF_1 = {
-    BAD_NAME_1 => {
-    }
+  NIL = nil
+  
+  SCALAR_1 = "some scalar"
+  
+  LIST_1 = [ "a", "b", "c" ]
+  
+  VALID_NAME = "SomeComponent"
+  
+  VALID_PARENT = "ParentComponent"
+  
+  LONG_HASH = {
+    "key1" => "value1",
+    "key2" => "value2"
+  }
+  
+  HASH_MISSING_NAME = {
+    "" => {}
+  }
+  
+  HASH_BAD_NAME = {
+    "this name is invalid" => {}
+  }
+  
+  HASH_MISSING_PARENT = {
+    VALID_NAME + " > " => {}
+  }
+  
+  HASH_BAD_PARENT = {
+    VALID_NAME + " > bad parent" => {}
+  }
+  
+  HASH_OF_NIL = {
+    VALID_NAME => NIL
+  }
+  
+  HASH_OF_SCALAR = {
+    VALID_NAME => SCALAR_1
+  }
+  
+  HASH_OF_LIST = {
+    VALID_NAME => LIST_1
+  }
+  
+  HASH_OF_HASH = {
+    VALID_NAME => {}
+  }
+  
+  PROPERTIES = {
+    "property1" => "value1",
+    "property2" => "value2",
+    "property3" => "value3",
+  }
+  
+  CHILDREN = [
+    "child1",
+    "child2",
+    "child3"
+  ]
+  
+  PROPERTIES_WITH_CHILDREN = {
+    "property1" => "value1",
+    "property2" => "value2",
+    "property3" => "value3",
+    "children" => CHILDREN
+  }
+  
+  HASH_OF_PROPS = {
+    VALID_NAME => PROPERTIES
+  }
+  
+  HASH_WITH_PARENT = {
+    VALID_NAME + " > " + VALID_PARENT => {}
   }
   
   #-----------------------------------------------------------------------------
@@ -45,6 +114,7 @@ describe ClassParser do
   #------------------------------
   
   before(:all) do
+    @class_parser = ClassParser.new( nil, nil )
   end
   
   #------------------------------
@@ -79,37 +149,157 @@ describe ClassParser do
   #   hash of hash
   #
   
-  it "should reject nil"
-  
-  it "should reject scalar"
-  
-  it "should reject list"
-  
-  it "should reject hash longer than one element"
-  
-  it "should reject missing name"
-  
-  it "should reject malformed name"
-  
-  it "should reject empty parent name"
-  
-  it "should reject malformed parent name"
-  
-  it "should reject hash of scalar"
-  
-  it "should reject hash of list"
+  it "should reject nil" do
+    begin
+      @class_parser.parse_yaml( NIL )
+    rescue Exceptions::DefinitionMalformed
+      success = true
+    end
+    
+    success.should == true
+  end
   
   
-  it "should accept valid empty hash"
+  it "should reject scalar" do
+    begin
+      @class_parser.parse_yaml( SCALAR_1 )
+    rescue Exceptions::DefinitionMalformed
+      success = true
+    end
+    
+    success.should == true
+  end
   
-  it "should accept valid hash of properties"
+  
+  it "should reject list" do
+    begin
+      @class_parser.parse_yaml( LIST_1 )
+    rescue Exceptions::DefinitionMalformed
+      success = true
+    end
+    
+    success.should == true
+  end
   
   
-  it "should parse parent names in the form '> Parent'"
+  it "should reject hash longer than one element" do
+    begin
+      @class_parser.parse_yaml( LONG_HASH )
+    rescue Exceptions::DefinitionMalformed
+      success = true
+    end
+    
+    success.should == true
+  end
   
-  it "should interpret the inner hash as properties"
   
-  it "should interpret the 'children' property as children"
+  it "should reject missing name" do
+    begin
+      @class_parser.parse_yaml( HASH_MISSING_NAME )
+    rescue Exceptions::DefinitionMalformed
+      success = true
+    end
+    
+    success.should == true
+  end
+  
+  
+  it "should reject malformed name" do
+    begin
+      @class_parser.parse_yaml( HASH_BAD_NAME )
+    rescue Exceptions::DefinitionMalformed
+      success = true
+    end
+    
+    success.should == true
+  end
+  
+  
+  it "should reject empty parent name" do
+    begin
+      @class_parser.parse_yaml( HASH_MISSING_PARENT )
+    rescue Exceptions::DefinitionMalformed
+      success = true
+    end
+    
+    success.should == true
+  end
+  
+  
+  it "should reject malformed parent name" do
+    begin
+      @class_parser.parse_yaml( HASH_BAD_PARENT )
+    rescue Exceptions::DefinitionMalformed
+      success = true
+    end
+    
+    success.should == true
+  end
+  
+  
+  it "should reject hash of scalar" do
+    begin
+      @class_parser.parse_yaml( HASH_OF_SCALAR )
+    rescue Exceptions::DefinitionMalformed
+      success = true
+    end
+    
+    success.should == true
+  end
+  
+  
+  it "should reject hash of list" do
+    begin
+      @class_parser.parse_yaml( HASH_OF_LIST )
+    rescue Exceptions::DefinitionMalformed
+      success = true
+    end
+    
+    success.should == true
+  end
+  
+  
+  
+  it "should accept valid empty hash" do
+    
+    result = @class_parser.parse_yaml( HASH_OF_NIL )
+    
+  end
+  
+  
+  it "should accept a hash of a hash" do
+    
+    result = @class_parser.parse_yaml( HASH_OF_HASH )
+    
+  end
+  
+  
+  it "should parse names in the form 'Name > Parent'" do
+    
+    result = @class_parser.parse_yaml( HASH_WITH_PARENT )
+    
+    result.name.should = VALID_NAME
+    result.parent.should = VALID_PARENT
+    
+  end
+  
+  
+  it "should interpret the inner hash as properties" do
+    
+    result = @class_parser.parse_yaml( HASH_OF_PROPS )
+    
+    result.properties.should == PROPERTIES
+    
+  end
+  
+  it "should interpret the 'children' property as children" do
+    
+    result = @class_parser.parse_yaml( HASH_WITH_CHILDREN )
+    
+    result.children.should = CHILDREN
+    result.properties.should = PROPERTIES
+    
+  end
   
   
 end
