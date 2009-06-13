2f45dc7980ff792922c0a86cf1936056c5c8e9d1
Will Madden <w.a.madden@gmail.com>
2009-06-13 15:55:57 +1000

    Finished DefinitionParser unit tests.

--- spec/DefinitionParser_spec.rb
+++ spec/DefinitionParser_spec.rb
@@ -21,13 +21,148 @@ require File.expand_path( 'src/InputParser.rb' )
 include Bob
 
 
+class DummyInputParser
+  
+  attr_accessor :called, :inputs
+  
+  def initialize
+    @inputs = []
+  end
+  
+  def parse_yaml( yaml )
+    @called = true
+    @inputs += yaml
+    
+    return DIP_PREFIX
+  end
+  
+end
+
+class DummyClassParser
+  
+  attr_accessor :called, :yaml, :component_class
+  
+  def initialize()
+    @called = false
+  end
+  
+  def parse_yaml( yaml, component_class )
+    @called = true
+    
+    @yaml = yaml
+    @component_class = component_class
+    
+    return component_class
+  end
+  
+end
+
+class DummyDefinitionHelper
+  
+  attr_accessor :called, :class_id
+  
+  def get_class( class_id )
+    @called = true
+    @class_id = class_id
+    
+    return nil
+  end
+  
+end
+
 describe DefinitionParser do
   
-  it "should use the class parser to parse the file"
+  #-----------------------------------------------------------------------------
+  #  
+  #  Input Constants
+  #  
+  #-----------------------------------------------------------------------------
   
-  it "should use the input parser to parse the class's values"
+  PARENT_NAME = "SomeParent"
+  COMPONENT_NAME = "SomeComponent"
+  COMPONENT_CHILDREN = [
+    "child1",
+    "child2",
+    "child3"
+  ]
+  COMPONENT_PROPS = {
+    "prop1" => "value1",
+    "prop2" => "value2",
+    "prop3" => "value3",
+    "children" => COMPONENT_CHILDREN      
+  }
   
-  it "should use the definition helper to get the parent"
+  EXPECTED_INPUTS = [
+    "value1",
+    "value2",
+    "value3",
+    "child1",
+    "child2",
+    "child3"
+  ]
+  
+  DEFINITION = {
+    COMPONENT_NAME + " < " + PARENT_NAME => COMPONENT_PROPS
+  }
+  
+  #-----------------------------------------------------------------------------
+  #  
+  #  Set up, tear down
+  #  
+  #-----------------------------------------------------------------------------
+  
+  #------------------------------
+  #  before(:each)
+  #------------------------------
+  
+  before(:each) do
+    @dummy_dh = DummyDefinitionHelper.new
+    @dummy_cp = DummyClassParser.new
+    @dummy_ip = DummyInputParser.new
+    
+    @definition_parser = 
+      DefinitionParser.new( @dummy_dh, @dummy_cp, @dummy_ip )
+  end
+  
+  #-----------------------------------------------------------------------------
+  #  
+  #  Tests
+  #  
+  #-----------------------------------------------------------------------------
+  
+  it "should use the class parser to parse the file" do
+    
+    component_class = ComponentClass.new
+    
+    @definition_parser.parse_yaml( DEFINITION, component_class )
+    
+    @dummy_cp.called.should == true
+    @dummy_cp.component_class.should == component_class
+    @dummy_cp.yaml.should == DEFINITION
+    
+  end
+  
+  it "should use the input parser to parse the class's values" do
+    
+    component_class = ComponentClass.new
+    
+    @definition_parser.parse_yaml( DEFINITION, component_class )
+    
+    @dummy_ip.called.should == true
+    @dummy_ip.inputs.should == EXPECTED_INPUTS
+    
+  end
+  
+  it "should use the definition helper to get the parent" do
+    
+    component_class = ComponentClass.new
+    
+    @definition_parser.parse_yaml( DEFINITION, component_class )
+    
+    @dummy_dh.called.should == true
+    @dummy_dh.class_id.should == PARENT_NAME
+    
+  end
   
 end
 
